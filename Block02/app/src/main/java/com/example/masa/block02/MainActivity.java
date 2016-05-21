f9e7e41f2d22231cacdf1c34014118b631a4b0e5
Masanori Igarashi <masa.iga64@gmail.com>
2016-05-21 13:20:33 +0900

    Add item click listener

--- Block02/app/src/main/java/com/example/masa/block02/MainActivity.java
+++ Block02/app/src/main/java/com/example/masa/block02/MainActivity.java
@@ -2,8 +2,11 @@ package com.example.masa.block02;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,10 +15,19 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ListView listViewAnimals = (ListView) findViewById(R.id.listView);
-        ArrayAdapter<CharSequence> adapterAnimals = ArrayAdapter.createFromResource(this,
+        final ArrayAdapter<CharSequence> adapterAnimals = ArrayAdapter.createFromResource(this,
                 R.array.animalsArray,
                 android.R.layout.simple_list_item_1);
         listViewAnimals.setAdapter(adapterAnimals);
 
+        listViewAnimals.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                String message = getString(R.string.toastMessage) + adapterAnimals.getItem(i);
+                Toast.makeText(getApplicationContext(),
+                        message,
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
